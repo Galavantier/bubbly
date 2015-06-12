a6233bd45e900f3c11e4d5c3f720e21db30190d1
Ray Imber <ray.imber@galavantier.com>
2015-06-12 14:55:10 -0700

    Added the pixi-js render engine.

--- src/main.js
+++ src/main.js
@@ -1,18 +1,2 @@
 (function () {
-  var theProtoObj = {};
-  var handler = a => a + 1;
-
-  var obj = {
-      // __proto__
-      __proto__: theProtoObj,
-      // Shorthand for ‘handler: handler’
-      handler,
-      // Methods
-      toString() {
-       // Super calls
-       return "d " + super.toString();
-      },
-      // Computed (dynamic) property names
-      [ "prop_" + (() => 42)() ]: 42
-  };
 })();
