6f4c94844466a4f7da7794c9e7f9f684e7fbbb65
Ray Imber <ray.imber@galavantier.com>
2015-06-13 15:59:34 -0700

    Cleared out everything. Starting with a blank slate.

--- src/assemblages.js
+++
@@ -1,9 +0,0 @@
-ECS.Assemblages = {
-  PhysicalThing : function(entity) {
-    entity.addComponent( new ECS.Components.Position() );
-    entity.addComponent( new ECS.Components.Rotation() );
-    entity.addComponent( new ECS.Components.PhysicsShape() );
-    entity.addComponent( new ECS.Components.Sprite() );
-    return entity;
-  }
-};
